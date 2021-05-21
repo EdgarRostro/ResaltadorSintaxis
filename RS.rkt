5dd15e72394a317d2fb92d3a274e81657d713419
EdgarRostro <edgar_rostro@outlook.com>
2021-05-21 18:20:47 -0500

    21/May

--- RS.rkt
+++ RS.rkt
@@ -18,76 +18,76 @@ false, true, null  false true null
     (string-append (car (regexp-match #px"^[\\w,\\s-]+" in-file-path)) ".html")
     #:exists 'truncate)
 
-  (let loop ([file (file->string in-file-path)]) 
+  (let loop ([file (file->string in-file-path)] [output-file (string-append (car (regexp-match #px"^[\\w,\\s-]+" in-file-path)) ".html")]) 
     (cond 
-      [(regexp-match? #px"[\\{\\}]" file) 
+      [(regexp-match? #px"^[{}]" file)
         (display-to-file 
-          (string-append "\t<div class=\"texto\">" (car (regexp-match #px"[{}]" file)) "</div>\n")
-          (string-append (car (regexp-match #px"^[\\w,\\s-]+" in-file-path)) ".html")
+          (string-append "\t<div class=\"Parentesis\">" (car (regexp-match #px"^[{}]" file)) "</div>\n")
+          output-file
           #:exists 'append)
-        (loop (substring file (string-length (car (regexp-match #px"[{}]"
-        file)))))]
+        (loop (substring file (string-length (car (regexp-match #px"^[{}]"
+        file)))) output-file)]
 
-      [(regexp-match? #px"[\\[\\]]" file) 
+      [(regexp-match? #px"^[\\[\\]]" file) 
         (display-to-file 
-          (string-append "\t<div class=\"texto\">" (car (regexp-match #px"[\\[\\]]" file)) "</div>\n")
-          (string-append (car (regexp-match #px"^[\\w,\\s-]+" in-file-path)) ".html")
+          (string-append "\t<div class=\"Corchetes\">" (car (regexp-match #px"^[\\[\\]]" file)) "</div>\n")
+          output-file
           #:exists 'append)
-        (loop (substring file (string-length (car (regexp-match #px"[\\[\\]]" file)))))]
+        (loop (substring file (string-length (car (regexp-match #px"^[\\[\\]]" file)))) output-file)]
 
-      [(regexp-match? #px"\\," file) 
+      [(regexp-match? #px"^\\," file) 
         (display-to-file 
-          (string-append "\t<div class=\"texto\">" (car (regexp-match #px"\\," file)) "</div>\n")
-          (string-append (car (regexp-match #px"^[\\w,\\s-]+" in-file-path)) ".html")
+          (string-append "\t<div class=\"Coma\">" (car (regexp-match #px"^\\," file)) "</div>\n")
+          output-file
           #:exists 'append)
-        (loop (substring file (string-length (car (regexp-match #px"\\," file)))))]
+        (loop (substring file (string-length (car (regexp-match #px"^\\," file)))) output-file )]
 
-      [(regexp-match? #px"\\:" file) 
+      [(regexp-match? #px"^\\:" file) 
         (display-to-file 
-          (string-append "\t<div class=\"texto\">" (car (regexp-match #px"\\:" file)) "</div>\n")
-          (string-append (car (regexp-match #px"^[\\w,\\s-]+" in-file-path)) ".html")
+          (string-append "\t<div class=\"DosP\">" (car (regexp-match #px"^\\:" file)) "</div>\n")
+          output-file
           #:exists 'append)
-        (loop (substring file (string-length (car (regexp-match #px"\\:" file)))))]
+        (loop (substring file (string-length (car (regexp-match #px"^\\:" file)))) output-file )]
 
-      [(regexp-match? #px"\\d+" file) 
+      [(regexp-match? #px"^[\\d\\.Ee+-]+\\d" file) 
         (display-to-file 
-          (string-append "\t<div class=\"texto\">" (car (regexp-match #px"\\d+" file)) "</div>\n")
-          (string-append (car (regexp-match #px"^[\\w,\\s-]+" in-file-path)) ".html")
+          (string-append "\t<div class=\"Numero\">" (car (regexp-match #px"^[\\d\\.Ee+-]+\\d" file)) "</div>\n")
+          output-file
           #:exists 'append)
-        (loop (substring file (string-length (car (regexp-match #px"\\d+" file)))))]
+        (loop (substring file (string-length (car (regexp-match #px"^[\\d\\.Ee+-]+\\d" file)))) output-file )]
 
-      [(regexp-match? #px"false|true|null" file) 
+      [(regexp-match? #px"^false|^true|^null" file) 
         (display-to-file 
-          (string-append "\t<div class=\"texto\">" (car (regexp-match #px"false|true|null" file)) "</div>\n")
-          (string-append (car (regexp-match #px"^[\\w,\\s-]+" in-file-path)) ".html")
+          (string-append "\t<div class=\"Bool\">" (car (regexp-match #px"^false|true|null" file)) "</div>\n")
+          output-file
           #:exists 'append)
-        (loop (substring file (string-length (car (regexp-match #px"false|true|null" file)))))]
+        (loop (substring file (string-length (car (regexp-match #px"^false|true|null" file)))) output-file )]
 
-      [(regexp-match? #px"(\"[\\w,\\s-]+\")\\s*\\:" file) 
+      [(regexp-match? #px"^(\"[\\w,.:()+;*/\\s-]+\")\\s*\\:" file) 
         (display-to-file 
-          (string-append "\t<div class=\"texto\">" (car (regexp-match #px"(\"[\\w,\\s-]+\")\\s*\\:" file)) "</div>\n")
-          (string-append (cdr (regexp-match #px"^[\\w,\\s-]+" in-file-path)) ".html")
+          (string-append "\t<div class=\"Text1\">" (cadr (regexp-match #px"^(\"[\\w,.:()+;*/\\s-]+\")\\s*\\:" file)) "</div>\n")
+          output-file
           #:exists 'append)
-        (loop (substring file (string-length (car (regexp-match #px"(\"[\\w,\\s-]+\")\\s*\\:" file)))))]
+        (loop (substring file (string-length (cadr (regexp-match #px"^(\"[\\w,.:()+;*/\\s-]+\")\\s*\\:()" file)))) output-file )]
       
-      [(regexp-match? #px"\"[\\w,\\s-]+\"" file) 
+      [(regexp-match? #px"^\"[\\w,.:()+;*/\\s-]+\"" file) 
         (display-to-file 
-          (string-append "\t<div class=\"texto\">" (car (regexp-match #px"\"[\\w,\\s-]+\"" file)) "</div>\n")
-          (string-append (car (regexp-match #px"^[\\w,\\s-]+" in-file-path)) ".html")
+          (string-append "\t<div class=\"Text\">" (car (regexp-match #px"^\"[\\w,.:()+;*/\\s-]+\"" file)) "</div>\n")
+          output-file
           #:exists 'append)
-        (loop (substring file (string-length (car (regexp-match #px"\"[\\w,\\s-]+\"" file)))))]
+        (loop (substring file (string-length (car (regexp-match #px"^\"[\\w,.:()+;*/\\s-]+\"" file)))) output-file )]
 
-        [(regexp-match? #px"\\s+" file) 
+        [(regexp-match? #px"^\\s+" file) 
         (display-to-file 
-          (string-append "\n\t<div class=\"texto\">" (car (regexp-match #px"\\s+" file)) "</div>")
-          (string-append (car (regexp-match #px"^[\\w,\\s-]+" in-file-path)) ".html")
+          (string-append "\t<div class=\"Espacio\">" (car (regexp-match #px"^\\s+" file)) "</div>\n")
+          output-file
           #:exists 'append)
-        (loop (substring file (- 1 (caar (regexp-match-positions #px"\\s+" file)))))]
+        (loop (substring file (string-length (car (regexp-match #px"^\\s+" file)))) output-file )]
         
         [(zero? (string-length file)) 
           (display-to-file 
             "</body>\n</html>"
-            (string-append (car (regexp-match #px"^[\\w,\\s-]+" in-file-path)) ".html")
+            output-file
             #:exists 'append)]
         )))
 
